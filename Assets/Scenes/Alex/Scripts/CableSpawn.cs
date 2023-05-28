11ba08eb2bfd8c04483bc419898183fd16e56252
AlexP-Coding <alexandra.pato@tecnico.ulisboa.pt>
2023-05-28 19:05:55 +0100

    Cable - Base script for cable creation/spawning

---
+++ Assets/Scenes/Alex/Scripts/CableSpawn.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cablePartPrefab, parentObject;
+
+    [SerializeField]
+    [Range(1, 1000)]
+    int length = 3;
+
+    [SerializeField]
+    [Range(1, 1000)]
+    // Distance between cable parts
+    float cablePartDistance = 0.21f; 
+
+    [SerializeField]
+    bool reset, spawn, snapFirst, snapLast;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reset)
+        {
+            foreach (GameObject cablePart in GameObject.FindGameObjectsWithTag("CablePart"))
+            {
+                Destroy(cablePart);
+            }
+
+            reset = false;
+        }
+
+        if (spawn)
+        {
+            Spawn();
+            spawn = false;
+        }
+    }
+
+
+    public void Spawn()
+    {
+        int count = (int)(length / cablePartDistance);
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject cablePart;
+
+            cablePart = Instantiate(cablePartPrefab,
+                                    new Vector3(
+                                        transform.position.x,
+                                        transform.position.y + cablePartDistance * (i + 1),
+                                        transform.position.z
+                                    ),
+                                    Quaternion.identity,
+                                    parentObject.transform
+                        );
+
+            cablePart.transform.eulerAngles = new Vector3(180, 0, 0);
+
+            cablePart.name = parentObject.transform.childCount.ToString();
+
+            if (i == 0)
+            {
+                Destroy(cablePart.GetComponent<CharacterJoint>());
+            }
+            else
+            {
+                cablePart.GetComponent<CharacterJoint>().connectedBody =
+                    parentObject.transform
+                    .Find((parentObject.transform.childCount - 1).ToString())
+                    .GetComponent<Rigidbody>();
+            }
+        }
+
+        if (snapLast)
+        {
+            parentObject.transform.Find((parentObject.transform.childCount).ToString()).GetComponent<Rigidbody>().constraints
+                = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
