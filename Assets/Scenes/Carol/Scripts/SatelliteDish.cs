a8f7a915055f02e361ba0118a76c0e949ee9c6c4
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-06-16 15:33:08 +0100

    inventory on console working needs UI

--- Assets/Scenes/Carol/Scripts/SatelliteDish.cs
+++ Assets/Scenes/Carol/Scripts/SatelliteDish.cs
@@ -7,22 +7,14 @@ using Scenes.Carol.Scripts;
 public class SatelliteDish : MonoBehaviour, ICollectible
 {
     
-    [SerializeField]
-    private GameObject umpa_ground;
-    
-    [SerializeField]
-    private GameObject neil_ground;
-    
     public static event CollectibleAction OnCollect;
     public delegate void CollectibleAction(ItemData itemData);
-    public ItemData itemData;
+    public ItemData dishData;
+  
     public void Collect()
     {
-        if (neil_ground == null && umpa_ground == null)
-        {
-            Destroy(gameObject);
-            OnCollect?.Invoke(itemData);
-        }
-      
+        OnCollect?.Invoke(dishData);
+        Debug.Log("Satellite Dish Collected");
+        Destroy(gameObject);
     }
 }
