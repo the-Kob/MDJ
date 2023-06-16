048f5b91d4df8f7693b7ad4a6ada5db2ce3c6db2
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-06-16 15:40:23 +0100

    inventory working for puzzle

--- Assets/Scenes/Carol/Scripts/SatelliteDish.cs
+++ Assets/Scenes/Carol/Scripts/SatelliteDish.cs
@@ -6,15 +6,21 @@ using Scenes.Carol.Scripts;
 
 public class SatelliteDish : MonoBehaviour, ICollectible
 {
-    
+    [SerializeField]
+    private GameObject neil_ground;
+    [SerializeField]
+    private GameObject umpa_ground;
     public static event CollectibleAction OnCollect;
     public delegate void CollectibleAction(ItemData itemData);
     public ItemData dishData;
   
     public void Collect()
     {
-        OnCollect?.Invoke(dishData);
-        Debug.Log("Satellite Dish Collected");
-        Destroy(gameObject);
+        if (neil_ground == null && umpa_ground == null)
+        {
+            OnCollect?.Invoke(dishData);
+            Debug.Log("Satellite Dish Collected");
+            Destroy(gameObject);
+        }
     }
 }
