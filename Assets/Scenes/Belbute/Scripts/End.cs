6e9a72cd8599afbf7d495537a33f29dbe43737e9
carolinabras <carolina.bras@tecnico.ulisboa.pt>
2023-06-18 18:26:29 +0100

    end scene if players touch "end object"
    final WITHOUT CAT

---
+++ Assets/Scenes/Belbute/Scripts/End.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Neil" || other.gameObject.tag == "Umpa")
+        {
+            Debug.Log("End of game");
+             // load scene with index + 1
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
